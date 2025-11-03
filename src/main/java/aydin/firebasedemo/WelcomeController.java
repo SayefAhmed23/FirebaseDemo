f6987d21b94f395395adfac05717464f48aa0453
Sayef <syiahmed31@gmail.com>
2025-11-02 19:59:01 -0500

    part 2 - updated again

--- src/main/java/aydin/firebasedemo/WelcomeController.java
+++ src/main/java/aydin/firebasedemo/WelcomeController.java
@@ -8,13 +8,13 @@ public class WelcomeController {
 
     @FXML
     private void onRegister(ActionEvent event) throws IOException {
-        // When user clicks "Register", go to your existing Primary.fxml screen
+
         DemoApp.setRoot("primary");
     }
 
     @FXML
     private void onSignIn(ActionEvent event) throws IOException {
-        // When user clicks "Sign In", go to your existing Secondary.fxml screen
+
         DemoApp.setRoot("secondary");
     }
 }
